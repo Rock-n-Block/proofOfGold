5c5cd0b02b30f549be30e43f98eacfc806d0c4e0
Gleb Chistyakov <gleb@MacBook-Pro-Gleb.local>
2021-01-25 18:25:10 +0300

    fix info card

--- src/components/InfoCard/index.tsx
+++ src/components/InfoCard/index.tsx
@@ -20,7 +20,7 @@ const infoCard: React.FC<infoCard> = ({ centered, img, title, text, type }) => {
       })}>
       <div className="i-card__wrapper">
         <img src={img} alt="" className="i-card__icon" />
-        <div className="i-card__title h3">{title}</div>
+        {title && <div className="i-card__title h3">{title}</div>}
         <div className="i-card__text text">{text}</div>
       </div>
     </div>
